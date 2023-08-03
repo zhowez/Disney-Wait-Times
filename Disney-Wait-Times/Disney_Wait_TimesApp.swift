47b5cbe49e014040bc4722ee9c7e751e262853e9
James Howes <zhowes14@icloud.com>
2023-08-03 15:26:48 -0400

    Initial Commit

---
+++ Disney-Wait-Times/Disney_Wait_TimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Disney_Wait_TimesApp.swift
+//  Disney-Wait-Times
+//
+//  Created by Zach Howes on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Disney_Wait_TimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Disney-Wait-TimesUITests/Disney_Wait_TimesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Disney_Wait_TimesUITestsLaunchTests.swift
+//  Disney-Wait-TimesUITests
+//
+//  Created by Zach Howes on 8/3/23.
+//
+
+import XCTest
+
+final class Disney_Wait_TimesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
